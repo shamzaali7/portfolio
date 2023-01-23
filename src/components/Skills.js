8e1e2e9e1e2d11f2e4db4886dd2b749991109537
Hamzaali7 <Hamzaali7@yahoo.com>
2023-01-22 19:50:55 -0500

    About-Me page updated and font changed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,7 @@ function Skills(){
                 <div className="display-skills">
                     <div className="holder-skills about-description">
                     <p><h6 className="skill-type"><span className="names">LANGUAGES</span> </h6> Javascript, SQL, Python, HTML, CSS, Java</p>
-                    <p><h6 className="skill-type"><span className="names">PROFICIENCIES</span></h6> Full-Stack Web Development, Front-End Web Development, React, Node.js, Express, MongoDB, NoSQL, SQL, Django, Git, Mocha, Chai, Heroku, Vercel, Fly, Github Pages, Postgres, Database Architecture, Data Structures, Algorithms, Version Control, REST API, Linux/Unix, Optimization, Agile Scrum Team, Data Analysis, Web Scraping</p> 
+                    <p><h6 className="skill-type"><span className="names">PROFICIENCIES</span></h6> Full-Stack Web Development, Front-End Web Development, Back-End Web Development, React, Node.js, Express, MongoDB, NoSQL, SQL, Django, Git, Mocha, Chai, Heroku, Vercel, Fly, Github Pages, Postgres, Database Architecture, Data Structures, Algorithms, Version Control, REST API, Linux/Unix, Optimization, Agile Scrum Team, Data Analysis, Web Scraping</p> 
                     <p><h6 className="skill-type"><span className="names">TOOLS</span> </h6> Microsoft Excel, Microsoft Word, Microsoft PowerPoint, Windows 10, Github, zsh, Slack</p>
                     <p><h6 className="skill-type"><span className="names">INTERPERSONAL LANGUAGES</span> </h6> Fluent in Urdu, English, Hindi, Conversational in Punjabi and Spanish, and literate in Arabic</p>
                     </div>
