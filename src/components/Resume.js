6e110f38056941dd6924272c894a7dbdbaab185a
Hamzaali7 <Hamzaali7@yahoo.com>
2024-11-12 18:35:30 -0500

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./Resume.css"
+import resumeImg1 from '../images/Res pdf1.png';
+import resumeImg2 from '../images/Res pdf2.png';
+
 
 function Resume(){
     return(
         <div className="resume">
             <div></div>
             <div className="resume-d">
-                <img src="https://i.imgur.com/8AdCaGI.png" className="resume-img" alt="resume"/>
+                <img src={resumeImg1} className="resume-img" alt="resume1/2"/>
+                <img src={resumeImg2} className="resume-img" alt="resume2/2"/>
             </div>
             <div></div>
         </div>
